dba5e741d7c1617e1dc94aa0b2d7a4967291c1c3
Epitech <thibaud.levy@coding-academy.fr>
2019-02-18 10:57:27 +0100

    Modification de l'indentation et aération du code

--- sapin.c
+++ sapin.c
@@ -22,31 +22,21 @@ int total_width(int taille)//largeur des blocs
     {
       cpt_lignes = 0;
       while (cpt_lignes < ligne)
-        	{  //0<4
-            //1<4
-            //2<4
-            //3<4
+        	{ 
         	  nb_etoiles = nb_etoiles + 2;
-
-            //1=1+2
-            //3=3+2
-            //5=5+2
-            //7
         	  cpt_lignes++;
         	}
       ligne++;
             if (nbBlocs % 2 == 0)
               // Savoir si le on est sur un bloc pair
-               
                 {
                   etoiles_perdues = etoiles_perdues + 2;
                 }
             nb_etoiles = nb_etoiles - etoiles_perdues;
             
                 nbBlocs++;
-   }
+    }
  return (nb_etoiles + etoiles_perdues - 2);
-
 }
 
 void mon_sapin(int taille)	
@@ -109,48 +99,51 @@ void tronc(int taille)
   int largeur = taille;
   int hauteur = taille;
 
-  if (taille%2 == 0)
-  {
-    largeur = taille +1;
-  }
-
-  nbr_espaces = total_width(taille)/ 2 - (largeur/2);
-
-  while(cpt_hauteur<hauteur)
-  {
-    cpt_hauteur++;
-    cpt_espaces=0;
-    while(cpt_espaces<nbr_espaces)
+    if (taille%2 == 0)
     {
-      my_putchar(' ');
-      cpt_espaces++;
-    }
-      cpt_width=0;
-    while(cpt_width<largeur)
-    {
-      my_putchar('|');
-      cpt_width++;
+      largeur = taille +1;
     }
 
-    my_putchar('\n');
-  }
+    nbr_espaces = total_width(taille)/ 2 - (largeur/2);
+
+    while(cpt_hauteur<hauteur)
+    {
+      cpt_hauteur++;
+      cpt_espaces=0;
+      while(cpt_espaces<nbr_espaces)
+      {
+        my_putchar(' ');
+        cpt_espaces++;
+      }
+        cpt_width=0;
+        while(cpt_width<largeur)
+        {
+          my_putchar('|');
+          cpt_width++;
+        }
+
+      my_putchar('\n');
+    }
 }
   
   int	main(int argc, char **argv)
 {
   
-   if (argc != 2)
-   return 1;
+  if (argc != 2){
+  printf("Impossible\n");
+  return 1;
+  }
 
       int taille;
       taille = atoi(argv[1]);
-       if (taille<1){
 
+       if (taille<1){
         printf("Impossible\n");
         return 1;
        }
-       mon_sapin(taille);
-       tronc(taille);
+       
+  mon_sapin(taille);
+  tronc(taille);
       
   return 0;
 }
